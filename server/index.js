1aee1509fb999ed636d4b1ab41111c16830e7ab4
NHQuAn2k2 <mhoangquan0@gmail.com>
2023-10-22 10:22:35 +0700

    connect mysql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
 const port = 8080;
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "employees",
+});
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log("Connected!");
 });
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
