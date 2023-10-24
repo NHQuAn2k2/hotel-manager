529b6effeeb059f92f2ab06380d81806f94570ec
NHQuAn2k2 <mhoangquan0@gmail.com>
2023-10-24 10:47:14 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("./mysqlConfig").connect("localhost", "root", "123456", "employees");
-const userRepository = require("./user/userRepository");
 const user_router = require("./user/userController");
 const app = express();
 const port = 8080;
