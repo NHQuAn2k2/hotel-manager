c5c3a67cc6cba4b3df764e81e17ff4d360eea7d8
NHQuAn2k2 <mhoangquan0@gmail.com>
2023-11-20 10:00:05 +0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 function App() {
-  return (
-    <Routes>
-      <Route path="/" Component={HomePage} />
-    </Routes>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
